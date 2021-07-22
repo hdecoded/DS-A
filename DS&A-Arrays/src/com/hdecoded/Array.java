2be05af2d84784d51ef42d4c208d01aa98fe1dd3
hdecoded <hd@outlook.in>
2021-07-23 04:33:14 +0530

    Initial files Commit

---
+++ DS&A-Arrays/src/com/hdecoded/Array.java
@@ -0,0 +1,23 @@
+package com.hdecoded;
+
+public class Array {
+
+    int length = 0;
+    public Array(int length) {
+        length = this.length;
+    }
+
+    public void insert(int item) {
+    }
+
+    public void print() {
+    }
+
+    public void removeAt(int index) {
+    }
+
+    public int indexOf(int number) {
+        int index = 0;
+        return index;
+    }
+}
