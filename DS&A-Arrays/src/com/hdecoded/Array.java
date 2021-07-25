11df1cbb8954bf1c7252d8bc2f8c1f84aa789cd8
hdecoded <hd@outlook.in>
2021-07-26 04:55:11 +0530

    Dynamic Array Implementation Complete

--- DS&A-Arrays/src/com/hdecoded/Array.java
+++ DS&A-Arrays/src/com/hdecoded/Array.java
@@ -2,22 +2,53 @@ package com.hdecoded;
 
 public class Array {
 
-    int length = 0;
+    private int[] items;
+    private int counter = 0;
+
     public Array(int length) {
-        length = this.length;
+        items = new int[length];
     }
 
     public void insert(int item) {
+        if(items.length == counter){
+            int newItems[] = new int[2 * counter];
+
+            for (int i = 0;i < counter; i++){
+                newItems[i] = items[i];
+            }
+
+            items = newItems;
+        }
+        items[counter] = item;
+        counter++;
     }
 
     public void print() {
+        for (int i = 0 ; i < counter ; i ++) {
+            System.out.println(items[i]);
+        }
     }
 
     public void removeAt(int index) {
+        if (index < 0 || index >= counter){
+            throw new IllegalArgumentException();
+        }
+
+        for( int i = index ; i < counter; i++) {
+            items[i] = items[i + 1];
+
+            counter--;
+        }
     }
 
     public int indexOf(int number) {
         int index = 0;
-        return index;
+
+        for (int i = 0 ; i < counter ; i ++) {
+            if(number == items[i]){
+               return index;
+            }
+        }
+        return -1;
     }
 }
--- DS&A-Arrays/src/com/hdecoded/Main.java
+++ DS&A-Arrays/src/com/hdecoded/Main.java
@@ -6,8 +6,15 @@ public class Main {
         numbers.insert(10);
         numbers.insert(20);
         numbers.insert(30);
-        numbers.print();
-        numbers.removeAt(3);
-        System.out.println(numbers.indexOf(10));
+        numbers.insert(40);
+        numbers.insert(50);
+        numbers.insert(60);
+        numbers.insert(70);
+        numbers.insert(80);
+        numbers.insert(90);
+        numbers.removeAt(90);
+        System.out.println(numbers.indexOf(67));
+        System.out.println(numbers.indexOf(60));
+
     }
 }
