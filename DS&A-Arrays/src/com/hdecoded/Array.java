3645245125beb02535e1e9f2d9be6c26f425858b
hdecoded <hd@outlook.in>
2021-07-26 05:38:50 +0530

    Adding methods max, intersect, reverse and insertAt

--- DS&A-Arrays/src/com/hdecoded/Array.java
+++ DS&A-Arrays/src/com/hdecoded/Array.java
@@ -36,9 +36,8 @@ public class Array {
 
         for( int i = index ; i < counter; i++) {
             items[i] = items[i + 1];
-
-            counter--;
         }
+        counter--;
     }
 
     public int indexOf(int number) {
@@ -50,5 +49,48 @@ public class Array {
             }
         }
         return -1;
+    }
+
+    public int max (){
+        int max = items[0];
+        for(int i = 0; i < counter ;i++){
+            if(max < items[i]){
+                max = items[i];
+            }
+        }
+        return max;
+    }
+
+    public Array intersect(Array array){
+        Array intersect = new Array(4);
+        for (int i = 0  ; i < array.counter; i++){
+            for( int j = 0 ; j < this.counter; j++ ){
+                if(this.items[j] == array.items[i]){
+                    intersect.insert(this.items[j]);
+                }
+            }
+        }
+        return intersect;
+    }
+
+    public Array reverse() {
+        Array reverse = new Array(1);
+
+        for (int i = counter; i > 0 ; i--){
+            reverse.insert(items[i-1]);
+        }
+
+        return reverse;
+    }
+
+    public void insertAt(int item, int index) {
+        if (index < 0 || index >= counter){
+            throw new IllegalArgumentException();
+        }
+        counter++;
+        for( int i = counter ; i > index; i--) {
+            items[i-1] = items[i-2];
+        }
+        items[index] = item;
     }
 }
--- DS&A-Arrays/src/com/hdecoded/Main.java
+++ DS&A-Arrays/src/com/hdecoded/Main.java
@@ -12,9 +12,21 @@ public class Main {
         numbers.insert(70);
         numbers.insert(80);
         numbers.insert(90);
-        numbers.removeAt(90);
+//        System.out.println(numbers.counter);
+        numbers.removeAt(5);
+//        System.out.println(numbers.counter);
         System.out.println(numbers.indexOf(67));
-        System.out.println(numbers.indexOf(60));
+//        System.out.println(numbers.max());
+        Array numbers2 = new Array(4);
+        numbers2.insert(60);
+        numbers2.insert(70);
+        numbers2.insert(80);
+        numbers2.insert(90);
+        numbers2.intersect(numbers).print();
+        System.out.println();
+        numbers.reverse().print();
+
+        numbers.insertAt(60,5);
 
     }
 }
